584f93e43a7871666975e99557aa5ce8e233db88
Muhammad Junaid <hafizjunaidqazisb@gmail.com>
2026-04-17 22:28:15 +0500

    feat: implement gallery section with responsive layout and image grid

--- app/module/home/Gallery.tsx
+++ app/module/home/Gallery.tsx
@@ -1,20 +1,23 @@
-import { div } from "framer-motion/client";
 import React from "react";
 import { IoMdSearch } from "react-icons/io";
 
+interface GalleryImageProps {
+  src: string;
+  className?: string;
+}
 // image card ki settings as component bana ke or props ma use kren gay
 
-const GalleryImage = ({ src, className = "" }) => (
+const GalleryImage = ({ src, className = "" } : GalleryImageProps) => (
   <div
     className={`relative group overflow-hidden rounded-xl cursor-pointer ${className}`}
   >
     <img
       src={src}
       alt="Gallery Item"
-      className="w-full h-full object-cover transition-transform duration-500 group=hover:scale-110"
+      className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
     />
-    // overlay
-    <div className="absolute inset-0 bg-[#f79dc3]/60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center jusitify-center">
+    {/* // overlay */}
+    <div className="absolute inset-0 bg-[#f79dc3]/60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
       <IoMdSearch className="text-6xl text-white transform scale-50 group-hover:scale-100 transition-transform duration-300" />
     </div>
   </div>
@@ -28,7 +31,42 @@ const Gallery = () => {
       <div className="py-20 px-4 md:px-10 lg:px-20 bg-white min-h-screen">
 
         {/* sub for aligning items */}
-        <div className="ma-w-7xl mx-auto flex flex-col items-center ">
+        <div className="max-w-7xl h-auto mx-auto flex flex-col items-center justify-center ">
+
+{/* heading */}
+<div className="text-center mb-12">
+        <p className="italic font-medium text-[45px] md:text-[70px]"> Our Gallery</p>
+
+        {/* categories / responsive on mobile  */}
+        <div className="flex flex-wrap justify-center gap-6 md:gap-10 mt-4 overflow-x-auto pb-2">
+                <p className="italic transition duration-300 cursor-pointer text-[#f79dc3] text-[18px] md:text-[20px]">All</p>
+                {["occasion" , "Bakery item", "Cream type"].map((item) =>(
+                        <p key={item} className="italic transition duration-300 hover:text-[#f79dc3] cursor-pointer text-[18px] md:text-[20px]">
+                                {item}
+                        </p>
+                ))}
+        </div>
+</div>
+{/* gallery section in grids  */}
+<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full h-auto">
+        {/* column 1 */}
+        <div className="flex flex-col gap-4">
+                <GalleryImage src="/Media/gallery-1.jpg.jpeg" className="h-[300px]" />
+
+                <GalleryImage src="/Media/gallery-2.jpg.jpeg" className="h-[300px]" />
+        </div>
+
+        {/* column 2 */}
+        <div className="flex flex-col">
+                <GalleryImage src="/Media/gallery-3.jpg.jpeg" className="h-full min-h-[400px] lg:h-[616px]" />
+        </div>
+
+        {/* column 3 */}
+        <div className="flex flex-col gap-4">
+                <GalleryImage src="/Media/gallery-4.jpg.jpeg" className="h-[300px]" />
+                <GalleryImage src="/Media/gallery-5.jpg.jpeg" className="h-[300px]" />
+        </div>
+</div>
 
         </div>
 
